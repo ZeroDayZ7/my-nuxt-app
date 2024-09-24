ef1901f74a5535e848c120bb9374696ec0dc60f4
ZeroDayZ7 <ZeroDayZ7@proton.me>
2024-09-24 10:03:25 +0200

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './components/**/*.{vue,js,ts,jsx,tsx}',
-    './layouts/**/*.vue',
-    './pages/**/*.{vue,js,ts,jsx,tsx}',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
   ],
   theme: {
     extend: {},
